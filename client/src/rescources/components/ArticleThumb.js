4827112ed2a52e65fe9baa487f6054cdc032a418
Emil <isdragon112@gmail.com>
2018-05-18 13:52:51 +0200

    Added template for frontpage

--- client/src/rescources/components/ArticleThumb.js
+++ client/src/rescources/components/ArticleThumb.js
@@ -3,26 +3,57 @@ import React, { Component } from "react";
 export default class ArticleThumb extends Component {
   render() {
     return (
-      <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "1rem",
+          width: '100%'
+        }}
+      >
         <h4
-            id="article-thumb"
-            style={{
-              margin: "70% 0 0 2%",
-              padding: "1.5% 4%",
-              backgroundColor: "#ff565c",
-              position: 'absolute',
-              textTransform: 'uppercase',
-              color: '#fff',
-              fontSize: '1.4rem',
-              fontFamily: 'Oswald SemiBold, Tahoma, Geneva, Verdana, sans-serif'
-            }}
+          id="article-thumb"
+          style={{
+            margin: "45% 0 0 2.6%",
+            padding: "1.5% 4%",
+            backgroundColor: "#ff565c",
+            position: "absolute",
+            textTransform: "uppercase",
+            color: "#fff",
+            fontSize: "1rem",
+            fontFamily: "Oswald SemiBold, Tahoma, Geneva, Verdana, sans-serif",
+            alignSelf: "flex-start"
+          }}
+        >
+          {this.props.category}
+        </h4>
+        <img
+          src={this.props.img}
+          alt={this.props.title}
+          style={{   maxWidth: "90vw", alignSelf: "center" }}
+        />
+        <div style={{ alignItems: 'flex-start', margin: '0 4vw 3% 4vw', maxWidth: '90vw', width: '100%' }} >
+          <h3
+            style={{ 
+              fontFamily: "Lato Semibold, Tahoma, Geneva, Verdana, sans-serif",
+              fontSize: '1.2rem',
+              fontWeight: '600',
+              margin: '3% 0 1% 0'
+          }}
           >
             {this.props.title}
-          </h4>
-      <div>
-         <img src={this.props.img} alt={this.props.title} style={{ zIndex: '-1', maxWidth: '90%', alignSelf: 'center' }} />
-        <p>{this.props.subheader}</p>
-      </div>
+          </h3>
+          <p 
+            style={{
+              fontFamily: "Lato regular, Tahoma, Geneva, Verdana, sans-serif",
+              color: '#5f6165',
+              margin: '0 0 5% 0'
+            }}
+          >
+            {this.props.subheader}
+          </p>
+        </div>
       </div>
     );
   }
--- client/src/rescources/pages/Frontpage.js
+++ client/src/rescources/pages/Frontpage.js
@@ -4,12 +4,19 @@ import ArticleThumb from '../components/ArticleThumb';
 export default class Frontpage extends Component {
   render() {
     return (
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} >
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: '#f9fafc' }} >
         <ArticleThumb
-          title={"Det sker"}
-          subheader={"Det sker på skolen lige nu"}
+          category={"elevrådet"}
+          title={'Elevrådsmøde d. 18-05-18'}
+          subheader={"Se referatet fra elevrådsmødet der blev afhold d. 18 maj 2018"}
           img={"https://cdn.stocksnap.io/img-thumbs/960w/SD9TQOOKCR.jpg"}
         />
+        <ArticleThumb
+          category={"Festudvalget"}
+          title={"Festplan 2018"} 
+          subheader={"Nu er planen for de kommende fester i 2018 klar"}
+          img={"https://cdn.stocksnap.io/img-thumbs/960w/9MS3AC9IKD.jpg"}
+          />
       </div>
     );
   }
